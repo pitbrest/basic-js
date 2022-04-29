87aa4b8503153b5799576047a726573d917139ed
pitbrest <sasha.petrov@tut.by>
2022-04-29 11:07:01 +0300

    some commit

--- src/common-character-count.js
+++ src/common-character-count.js
@@ -12,10 +12,27 @@ const { NotImplementedError } = require('../extensions/index.js');
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
 function getCommonCharacterCount(/* s1, s2 */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+	throw new NotImplementedError('Not implemented');
+	// remove line with error and write your code here
 }
 
 module.exports = {
-  getCommonCharacterCount
+	getCommonCharacterCount
 };
+
+function getCommonCharacterCount(a, b) {
+
+	let sup1 = Array.from(a);
+	let sup2 = Array.from(b);
+	let resultArr = [];
+
+	for (let item of sup1) {
+		if (sup2.includes(item)) {
+			resultArr.push(item);
+			sup2.splice(sup2.indexOf(item), 1);
+		}
+	}
+	return resultArr.length;
+
+	// console.log(sup1);
+}
--- test/common-character-count.test.js
+++ test/common-character-count.test.js
@@ -7,11 +7,13 @@ it.optional = testOptional;
 Object.freeze(assert);
 
 describe('Common character count', () => {
-  it.optional('should return the number of common characters between the given strings', () => {
-    assert.strictEqual(getCommonCharacterCount('aabcc', 'adcaa'), 3);
-    assert.strictEqual(getCommonCharacterCount('zzzz', 'zzzzzzz'), 4);
-    assert.strictEqual(getCommonCharacterCount('abca', 'xyzbac'), 3);
-    assert.strictEqual(getCommonCharacterCount('', 'abc'), 0);
-    assert.strictEqual(getCommonCharacterCount('a', 'b'), 0);
-  });
+	it.optional('should return the number of common characters between the given strings', () => {
+		assert.strictEqual(getCommonCharacterCount('aabcc', 'adcaa'), 3);
+		assert.strictEqual(getCommonCharacterCount('zzzz', 'zzzzzzz'), 4);
+		assert.strictEqual(getCommonCharacterCount('abca', 'xyzbac'), 3);
+		assert.strictEqual(getCommonCharacterCount('', 'abc'), 0);
+		assert.strictEqual(getCommonCharacterCount('a', 'b'), 0);
+	});
 });
+
+
